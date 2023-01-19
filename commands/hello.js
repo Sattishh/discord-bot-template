d3534ffda1f69f886582005c0c0ae3ac2d1f0eff
Sattish Shewkani <spshewkani@gmail.com>
2023-01-19 22:16:25 +0800

    init

---
+++ commands/hello.js
@@ -0,0 +1,11 @@
+module.exports.run = async (bot, message, args) => {
+    
+}
+
+module.exports.config = {
+    name: "",
+    description: "",
+    usage: "",
+    accessableby: "",
+    aliases: []
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const fs = require('fs');
+
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online!`)
+});
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+    let prefix = '!'
+    if (!message.content.toLowerCase().startsWith(prefix)) return;
+        let messageArray = message.content.split(" ");
+        let cmd = messageArray[0];
+        let args = messageArray.slice(1);
+        let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+        if(commandfile) commandfile.run(bot,message,args)
+});
+
+bot.login()
